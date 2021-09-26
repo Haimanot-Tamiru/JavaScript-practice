0845dfbbcadbf6d3021c0bd3a3b1dd91fa251b9c
Haimanot Tamiru <chaniehaimanot@gmail.com>
2021-09-25 22:47:21 -0500

    day-2 branch

--- js/script.js
+++ js/script.js
@@ -4,4 +4,9 @@
 // Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati cupiditate enim necessitatibus harum, voluptas aut id est amet libero omnis voluptatibus fugiat mollitia eligendi rerum esse. Maiores dolorem quidem repudiandae.
 
 console.log('My name is Haimanot')
-console.log("My favorite food is kuanta firfir")
+console.log("My favorite food is kuanta firfir");
+
+
+console.log('Supercalifragilisticexpialidocious'.length);
+console.log(typeof(21));
+console.log(typeof(45464864684684n));
